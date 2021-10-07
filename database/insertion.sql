188b053861de77c2b3f27e95e59146e5210f3730
Robertlean <robert_lean@live.com.ar>
2021-10-07 01:29:11 -0300

    inserccion de colores a la tabla

---
+++ database/insertion.sql
@@ -0,0 +1,26 @@
+/* Insercion para la tabla colors */
+
+insert into colors (id, name, hexa)
+values(1, 'Blanco', '#ffffff');
+insert into colors (id, name, hexa)
+values(2, 'Gris', '#c0c0c0');
+insert into colors (id, name, hexa)
+values(3, 'Negro', '#000000');
+insert into colors (id, name, hexa)
+values(4, 'Rojo', '#ff0000');
+insert into colors (id, name, hexa)
+values(5, 'Marrón', '#800000');
+insert into colors (id, name, hexa)
+values(6, 'Olivo', '#808000');
+insert into colors (id, name, hexa)
+values(7, 'Verde Lima', '#00ff00');
+insert into colors (id, name, hexa)
+values(8, 'Verde', '#008000');
+insert into colors (id, name, hexa)
+values(9, 'Aqua', '#00fffff');
+insert into colors (id, name, hexa)
+values(10, 'Azul', '#0000ff');
+insert into colors (id, name, hexa)
+values(11, 'Fucsia', '#ff00ff');
+insert into colors (id, name, hexa)
+values(12, 'Purpura', '#800080');
